7d0b60b486ffde3afadd52d5636380ab12e82019
Atikah <atikahnazir6@gmail.com>
2023-12-07 16:17:32 +0800

    style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        FEOrange: "hsl(26, 100%, 55%)",
+        FEPaleOrange: "hsl(25, 100%, 94%)",
+        FEVeryDarkBlue: "hsl(220, 13%, 13%)",
+        FEDarkGrayishBlue: "hsl(219, 9%, 45%)",
+        FEGrayishBlue: "hsl(220, 14%, 75%)",
+        FELightGrayishBlue: "hsl(223, 64%, 98%)",
+        FEWhite: "hsl(0, 0%, 100%)",
+        FEBlackLightBox: "hsl(0, 0%, 0%)",
+      },
+      fontFamily: {
+        FEKumbhSans: ["Kumbh Sans"],
+      },
+    },
   },
   plugins: [],
 };
