fed6042b7346389df335242645de553e37467bc2
Atikah <atikahnazir6@gmail.com>
2023-12-08 17:40:16 +0800

    import image

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import InfoItem from "./components/InfoItem";
+import ImagePreview from "./components/ImagePreview";
 
 function App() {
   return (
     <>
       <div className="">
         <NavBar />
+        <ImagePreview />
+
         <InfoItem />
       </div>
     </>
--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import Images from "../images.json";
 
 function ImagePreview() {
   const [imageIndex, setImageIndex] = useState(0);
   const [next, setNext] = useState(false);
   const [previous, setPrevious] = useState(false);
+
+  return (
+    <div>
+      {Images &&
+        Images.map((item, index) => <img src={item.img} alt={item.id} />)}
+    </div>
+  );
 }
 
 export default ImagePreview;
