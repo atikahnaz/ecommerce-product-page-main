bc0c350828b0c69b9ff52a42f1ad4cffb58c6548
Atikah <atikahnazir6@gmail.com>
2023-12-29 14:59:10 +0800

    closeicon

--- src/components/CollapseMenu.jsx
+++ src/components/CollapseMenu.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import NavBar from "./NavBar";
+import CloseIcon from "../images/icon-close.svg";
 
 function Menu() {
   const [closeMenu, setCloseMenu] = useState(false);
@@ -13,7 +14,7 @@ function Menu() {
           <div className="fixed top-0 bg-FEBlackLightBox opacity-60 w-screen h-screen z-40"></div>
           <div className="fixed top-0 w-3/4 h-screen pl-4 bg-FEWhite opacity-100 z-50">
             <div className="py-4">
-              <img src="src/images/icon-close.svg" alt="" onClick={Close} />
+              <img src={CloseIcon} alt="" onClick={Close} />
             </div>
             <div className="pt-4 font-FEKumbhSans font-bold text-base">
               <div className="mb-4">Collections</div>
